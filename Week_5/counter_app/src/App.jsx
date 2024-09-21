5000266a74fc9690d57fe0235e70b19c4ee4c9c4
nitin611 <jhanitin906@gmail.com>
2024-09-21 17:41:40 +0530

    Counter application with react

---
+++ Week_5/counter_app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// state, components-
+
+
+function App() {
+  const [count,setCount]=useState(0);
+  
+  return (
+    <div>
+    <CounterButton count={count} setCount={setCount}></CounterButton>
+    </div>
+  )
+}
+// making our own components and calling these components from the main app-
+function CounterButton(props){
+
+  function onClickHandler(){
+    // re-rendering the setCount-
+    props.setCount(props.count+1)
+  }
+  return <button onClick={onClickHandler}>
+    Counter {props.count}
+  </button>
+
+}
+
+export default App
